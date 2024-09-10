2e4b94bb259d4dd4b4a7fefb9c6696a47e9ca71a
caesar <caesar.cuzco@jjay.cuny.edu>
2024-09-09 20:14:30 -0400

    add next-auth and modify the header component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
+import SessionWrapper from "@/components/SessionWrapper";
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
@@ -20,11 +21,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <Header />
-        {children}
-      </body>
-    </html>
+    <SessionWrapper>
+      <html lang="en">
+        <body>
+          <Header />
+          {children}
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Header() {
+  const { data: session } = useSession();
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
@@ -30,7 +34,18 @@ export default function Header() {
           className="bg-gray50 border border-gray-200 rouded text-sm w-full py-2 px-4 max-w-[210px]"
         />
         {/* menu items */}
-        <button class="text-sm font-semibold text-blue-500">Log In</button>
+        {session ? (
+          <img
+            src={session.user.image}
+            alt={session.user.name}
+            className="h-10 w-10 rounded-full cursor-pointer"
+            onClick={signOut}
+          />
+        ) : (
+          <button onClick={signIn} class="text-sm font-semibold text-blue-500">
+            Log In
+          </button>
+        )}
       </div>
     </div>
   );
