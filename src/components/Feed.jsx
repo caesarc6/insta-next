0fb42463bcee668311c2512dcc09260c388eb86d
caesarc <46427941+caesarc6@users.noreply.github.com>
2024-09-21 23:59:18 -0400

    make the feed section responsive

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,16 +1,15 @@
-import React from "react";
 import Posts from "@/components/Posts";
 import MiniProfile from "@/components/MiniProfile";
 
 export default function Feed() {
   return (
-    <main>
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
       {/* posts (left) */}
-      <section>
+      <section className="md:col-span-2">
         <Posts />
       </section>
       {/* mini profile (right) */}
-      <section>
+      <section className="hidden md:inline-grid md:col-span-1">
         <MiniProfile />
       </section>
     </main>
